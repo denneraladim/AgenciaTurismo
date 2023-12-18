ff4b1333b4c1fe793809dcd85010e8444ee8f079
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2023-12-18 19:42:20 -0300

    Criado o componente Card

--- src/pages/Passagens/index.jsx
+++ src/pages/Passagens/index.jsx
@@ -1,5 +1,12 @@
+import Card from "../../components/Card";
+
 export default function Passagens(){
     return (
-        <h1>Passagens</h1>
+       <>
+         <h1>Passagens Aéreas</h1>
+         <Card />
+         <Card />
+         <Card />
+       </>
     )
 }
